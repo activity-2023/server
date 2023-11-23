3388e3f9afd369d513e50ca762270b30f44e27af
Thomas REMY <contact.t.remy777@gmail.com>
2023-11-23 02:46:22 +0100

    Add HibernateUtil class for managing the SessionFactory
    This commit adds a new file, `HibernateUtil.java`, which contains the implementation of a utility class for managing the Hibernate SessionFactory. The SessionFactory is initialized with annotated classes representing various entities in the application. If an error occurs during initialization, an exception is thrown and logged. The `getSessionFactory()` method provides access to the initialized SessionFactory instance.
    
    The purpose of this change is to provide a centralized way of managing the Hibernate SessionFactory, making it easier to work with database operations in the application.

---
+++ app/src/main/java/fr/cyu/depinfo/activity/server/HibernateUtil.java
@@ -0,0 +1,49 @@
+package fr.cyu.depinfo.activity.server;
+
+import fr.cyu.depinfo.activity.model.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.BootstrapServiceRegistryBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+    private static final Logger logger = LogManager.getLogger();
+
+    private static final SessionFactory SESSION_FACTORY;
+
+    static {
+        try {
+            SESSION_FACTORY = new MetadataSources(
+                    new StandardServiceRegistryBuilder(
+                            new BootstrapServiceRegistryBuilder()
+                                    .build()
+                    )
+                            .build())
+                    .addAnnotatedClass(Person.class)
+                    .addAnnotatedClass(User.class)
+                    .addAnnotatedClass(Parent.class)
+                    .addAnnotatedClass(Child.class)
+                    .addAnnotatedClass(Staff.class)
+                    .addAnnotatedClass(ExternalStaff.class)
+                    .addAnnotatedClass(InternalError.class)
+                    .addAnnotatedClass(Activity.class)
+                    .addAnnotatedClass(Building.class)
+                    .addAnnotatedClass(Room.class)
+                    .addAnnotatedClass(Event.class)
+                    .addAnnotatedClass(StaffPresence.class)
+                    .addAnnotatedClass(BuildingLog.class)
+                    .addAnnotatedClass(RoomLog.class)
+                    .buildMetadata()
+                    .buildSessionFactory();
+        } catch (Throwable e) {
+            logger.error("An error occurred when initializing the SessionFactory", e);
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return SESSION_FACTORY;
+    }
+}
