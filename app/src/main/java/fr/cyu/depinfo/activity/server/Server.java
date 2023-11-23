ba749d9fcd0cde867f19f6a03eb29134b6759c5f
Thomas REMY <contact.t.remy777@gmail.com>
2023-11-23 18:15:04 +0100

    Add Server class for handling network connections
    This commit adds a new file `Server.java` which contains the implementation of the `Server` class. The `Server` class is responsible for handling network connections and starts a server socket to listen for incoming connections on a specified address and port. It uses a thread pool to handle multiple concurrent connections and logs information using Log4j. The `Server` class also initializes a Hibernate session factory for database operations.
    
    The `start()` method in the `Server` class starts the server socket and accepts incoming connections, creating a new thread to process each connection using the provided session factory.
    
    This change allows for easy creation of network servers with customizable address and port configurations.

---
+++ app/src/main/java/fr/cyu/depinfo/activity/server/Server.java
@@ -0,0 +1,50 @@
+package fr.cyu.depinfo.activity.server;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.SessionFactory;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private static final Logger logger = LogManager.getLogger();
+
+    private ThreadGroup g;
+    private NetworkThreadFactory ntFactory;
+    private ExecutorService service;
+
+    private int port = 15678;
+    private InetAddress addr = InetAddress.getByName("127.0.0.1");
+
+    private SessionFactory sessionFactory;
+
+    {
+        g = new ThreadGroup("NetworkThreads");
+        ntFactory = new NetworkThreadFactory(g);
+        service = Executors.newCachedThreadPool(ntFactory);
+
+        sessionFactory = HibernateUtil.getSessionFactory();
+    }
+
+    public Server() throws UnknownHostException {}
+
+    public Server(String addr, int port) throws UnknownHostException {
+        this.addr = InetAddress.getByName(addr);
+        this.port = port;
+    }
+
+    public void start() {
+        try (ServerSocket serverSocket = new ServerSocket(this.port, 50, this.addr)) {
+            logger.info("Server listening on {}:{}", this.addr.getHostAddress(), this.port);
+
+            while (true) {
+                this.service.submit(new NetworkProcess(serverSocket.accept(), sessionFactory));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
