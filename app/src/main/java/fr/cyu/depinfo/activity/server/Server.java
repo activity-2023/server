932ab2b13022fbafc132765ba00e02422a6f44d7
Thomas REMY <contact.t.remy777@gmail.com>
2023-11-24 18:52:55 +0100

    feat: Add error handling for client connection
    This commit adds error handling for when the server fails to connect to a client. If an exception occurs, it is logged and the server continues listening for new connections. Additionally, if a BindException occurs when starting the server, it will attempt to start on another port.

--- app/src/main/java/fr/cyu/depinfo/activity/server/Server.java
+++ app/src/main/java/fr/cyu/depinfo/activity/server/Server.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.hibernate.SessionFactory;
 
 import java.io.IOException;
+import java.net.BindException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.UnknownHostException;
@@ -19,6 +20,8 @@ public class Server {
 
     public static InetAddress SERVER_ADDRESS;
     public static int SERVER_PORT;
+
+    private boolean portRetry = false;
 
     private ThreadGroup g;
     private NetworkThreadFactory ntFactory;
@@ -40,10 +43,21 @@ public class Server {
 
     public void start() throws RuntimeException {
         try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT, 50, SERVER_ADDRESS)) {
-            logger.info("Server listening on {}:{}", SERVER_ADDRESS, SERVER_PORT);
+            logger.info("Server listening on {}:{}", SERVER_ADDRESS, serverSocket.getLocalPort());
 
             while (true) {
-                this.service.submit(new NetworkProcess(serverSocket.accept(), sessionFactory));
+                try {
+                    this.service.submit(new NetworkProcess(serverSocket.accept(), sessionFactory));
+                } catch (RuntimeException e) {
+                    logger.error("An error occurred when trying to connect to a client.", e);
+                }
+            }
+        } catch (BindException e) {
+            if (!portRetry) {
+                portRetry = true;
+                logger.error("An error occurred when starting the server. Trying another port...", e);
+                SERVER_PORT = 0;
+                start();
             }
         } catch (IOException e) {
             throw new RuntimeException("An error occurred when trying to start the server.", e);
