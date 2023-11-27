0012e3f598e0ad9ba04be7769f29537958ce84a6
Thomas REMY <contact.t.remy777@gmail.com>
2023-11-27 12:27:17 +0100

    Refactor Main.java for cleaner code structure
    - Remove unused imports and dependencies
    - Simplify the main method by removing unnecessary code

--- app/src/main/java/fr/cyu/depinfo/activity/Main.java
+++ app/src/main/java/fr/cyu/depinfo/activity/Main.java
@@ -1,20 +1,7 @@
 package fr.cyu.depinfo.activity;
 
 import fr.cyu.depinfo.activity.cli.ServerStart;
-import fr.cyu.depinfo.activity.model.User;
-import fr.cyu.depinfo.activity.server.HibernateUtil;
-import fr.cyu.depinfo.activity.server.Server;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.hibernate.SessionFactory;
-import org.tomlj.Toml;
-import org.tomlj.TomlParseResult;
 import picocli.CommandLine;
-
-import java.io.IOException;
-import java.net.UnknownHostException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Main {
     public static void main(String... args) {
